0970a95a5375099209fdbd6e5200790ff8528cff
Luis-bcruz <luis.labc95@gmail.com>
2016-10-05 13:06:10 +0100

    chaged the board to 8x8 again!

--- src/board.pl
+++ src/board.pl
@@ -6,26 +6,40 @@ board([[blue,red,blue,red,blue,red,blue,red],
        [blue,red,blue,red,blue,red,blue,red],
        [red,blue,red,blue,red,blue,red,blue],
        [blue,red,blue,red,blue,red,blue,red],
-       [red,blue,red,blue,red,blue,red,blue],
-       [blue,red,blue,red,blue,red,blue,red],
-       [red,blue,red,blue,red,blue,red,blue]]).
+       [red,blue,red,blue,red,blue,red,blue]
+       ]).
 
 % display board
 display_board([H|T]) :- 
         % how to display 1st line border?
-        display_line(H), nl, write(' --------------------------------- '), nl,
-        display_board(T),
-        display_board([]).
+        write('   ----------------------------------------------------------------- '), nl,
+        display_empty_line([]),
+        display_line(H), nl,
+        display_empty_line([]),
+        display_board(T).
+
+display_board([]):-
+        write('   ----------------------------------------------------------------- '), nl.
+
+
+% display_line([]):- write('      '), nl.
 
 % display line
 display_line([H|T]) :-
         symbol(H,S),
-        write(' | '), write(S), 
+        write('   |   '), write(S), 
         display_line(T).
 display_line([]) :-
-        write(' | ').
+        write('   |   ').
 
+%display empty line
+display_empty_line([]):-
+        write('   '),
+        write('|'), write('       '), write('|'), write('       '), write('|'), write('       '), write('|'),
+        write('       '), write('|'), write('       '), write('|'), write('       '), write('|'), 
+        write('       '), write('|'), write('       '), write('|'), nl.
+        
 % symbols
-symbol(red,'R').
-symbol(blue,'B').
+symbol(red,'r').
+symbol(blue,'b').
 symbol(empty,' ').
