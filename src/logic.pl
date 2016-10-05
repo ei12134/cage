b8d65bfb99f17480c296899778f3bef0dc2d4424
Luis-bcruz <luis.labc95@gmail.com>
2016-10-05 11:33:57 +0100

    added some moves in logic

---
+++ src/logic.pl
@@ -0,0 +1,10 @@
+% moves
+centering_move(row,col,dest_row,dest_col,board).
+
+adjoining_mode(row,col,dest_row,dest_col,board).
+
+jump(row,col,dest_row,dest_col,board).
+
+restriction_1(row,col,adj_row,adj_col,boad).
+
+restriction_2(row,col,adj_row,adj_col,board).
