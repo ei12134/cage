a0acb4b2c7ce8c1cb103a7cf614efddb03211f70
Luis-bcruz <luis.labc95@gmail.com>
2016-10-27 17:42:47 +0100

    started get coords

--- src/cage.pl
+++ src/cage.pl
@@ -7,3 +7,22 @@
 % program starting point
 %cage :- main_menu.
 cage :- board(B), display_board(B,8).
+
+
+% piece input functions 
+getPieceSrcCoord(SrcRow, SrcCol):-
+        write("Please enter the value of the row and col of the piece you whant to move and press <Enter>"), nl,
+        inputCoords(SrcRow,SrcCol), nl.
+
+getPieceDestCoord(DstRow, DstCol):-
+         write("Please enter the value of the row and col where you whant to move and press <Enter>"), nl,
+         inputCoords(DstRow,DstCol), nl.
+
+inputCoords(Row,Col):-
+        getInteger(R), getInteger(C), get_code(_), Row is R-1, Col is C-49.
+
+getInteger(Input):-
+        get_code(Temp),
+        Input is Temp-48.
+
+
--- src/logic.pl
+++ src/logic.pl
@@ -4,3 +4,12 @@ adjoining_mode(row,col,dest_row,dest_col,board).
 jump(row,col,dest_row,dest_col,board).
 restriction_1(row,col,adj_row,adj_col,boad).
 restriction_2(row,col,adj_row,adj_col,board).
+
+% not finished
+validateMove(SrcRow,SrcCol, DestRow, DestCol, Board, ResultBoard):-
+        validateCentering_move(SrcRow,SrcCol, DestRow, DestCol, Board, ResultBoard);
+        validateAdjoining_move(SrcRow,SrcCol, DestRow, DestCol, Board, ResultBoard);
+        validateJump(SrcRow,SrcCol, DestRow, DestCol, Board, ResultBoard);
+        invalidMove.
+
+                       
